6a79f3ed1dd4e4d2f3eab741a215c48f14793be4
clustor <jstrik@gmail.com>
2015-02-13 11:25:35 +0100

    Fixed scope issues
    fixed the scope issues and consolidated some functions

--- PSGraph.psm1
+++ PSGraph.psm1
@@ -1,7 +1,11 @@
-﻿#version 1.0
+﻿#version 2.1
 #jst
 #changelog v1.0 - Initial release
- 
+#changelog v2.0 - optimization
+#changelog v2.1
+#- consolidation of module using member methods
+#- fixed scope issues with this module
+
 [void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.DataVisualization")
 function New-PSGraphChart {
   [CmdletBinding()]
@@ -12,16 +16,16 @@ function New-PSGraphChart {
     [Parameter(Mandatory=$True)][string]$AxisYTitle,
     [Parameter(Mandatory=$false)][string]$AxisXInterval = 5,
     [Parameter(Mandatory=$True)][string]$ChartTitle
-    
+
   )
-  
+
   $chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart
   $chart.Width = $Width
   $chart.Height = $Height
   $chart.BackColor = [System.Drawing.Color]::White
-  
-  
-  # chart area 
+
+
+  # chart area
   $chartarea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
   $chartarea.Name = "ChartArea1"
   $chartarea.AxisY.Title = $AxisYTitle
@@ -29,16 +33,16 @@ function New-PSGraphChart {
   #$chartarea.AxisY.Interval = 100
   $chartarea.AxisX.Interval = $AxisXInterval
   $chart.ChartAreas.Add($chartarea)
-  
-  # legend 
+
+  # legend
   $legend = New-Object system.Windows.Forms.DataVisualization.Charting.Legend
   $legend.name = "Legend1"
-  
+
   $chart.Legends.Add($legend)
   [void]$chart.Titles.Add($ChartTitle)
   $chart.Titles[0].Font = "Arial,20pt"
   $chart.Titles[0].Alignment = "topLeft"
-  
+
   #add a scriptmethod to be able to save the chart to disk
   $chart | Add-Member -MemberType ScriptMethod -Name "SaveChart" -Value {
   Param(
@@ -48,60 +52,35 @@ function New-PSGraphChart {
   )
 
 
-  $this.SaveImage($FileName,$fileformat)
+  $this.SaveImage($FileName,$fileformat) |out-null
   if($OpenImages -eq $true){Invoke-Item $FileName }
 
+  }
+
+  #add a scriptmethod to be able to add a graph serie
+  $chart | Add-Member -MemberType Scriptmethod -Name "AddSerie" -Value {
+    Param(
+      [Parameter(Mandatory=$True)][string]$SerieName,
+      [Parameter(Mandatory=$false)][string]$Type="Line"
+    )
+      #$chart = Get-Variable -Name "$ChartName" -ValueOnly
+      $this.Series.Add($SerieName)
+      $this.Series[$SerieName].ChartType = $Type
+      $this.Series[$SerieName].IsVisibleInLegend = $true
+      $this.Series[$SerieName].BorderWidth  = 3
+      $this.Series[$SerieName].chartarea = "ChartArea1"
+
+  }
+
+  $chart | Add-Member -MemberType Scriptmethod -Name "AddSerieValues" -Value {
+    Param(
+      [Parameter(Mandatory=$True)][string]$SerieName,
+      [Parameter(Mandatory=$True)][string]$XValue,
+      [Parameter(Mandatory=$True)][string]$YValue
+    )
+    $this.Series[$SerieName].Points.addxy( $XValue , $YValue)  |Out-Null
   }
 
   $chart
 }
-
-#Takes object collection and adds a serie for each object piped into it
-function Add-PSGraphSerie {
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory=$True)][string]$Name,
-    [Parameter(Mandatory=$false)][string]$Type="Line",
-    [Parameter(Mandatory=$True)][string]$XPropertyName,
-    [Parameter(Mandatory=$True)][string]$YPropertyName,
-    [Parameter(Mandatory=$True)][string]$ChartName,
-    [Parameter(ValueFromPipeline=$True)][string[]]$datasource
-    
-  )
-  BEGIN {
-    
-    $chart = Get-Variable -Name "$ChartName" -ValueOnly
-    $chart.Series.Add($Name)
-    $chart.Series[$Name].ChartType = $Type
-    $chart.Series[$Name].IsVisibleInLegend = $true
-    $chart.Series[$Name].BorderWidth  = 3
-    $chart.Series[$Name].chartarea = "ChartArea1"
-    
-  }
-  
-  PROCESS{
-    
-    $chart.Series[$Name].Points.addxy( $_.($XPropertyName) , $_.($YPropertyName))  |Out-Null
-  }
-  
-  END {}
-}
-
-
-function Out-PSGraphImageFile
-{
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory=$True)][string]$FileName,
-    [Parameter(Mandatory=$false)][string]$fileformat="png",
-    [Parameter(Mandatory=$True)][string]$ChartName,
-    [Parameter(Mandatory=$false)][string]$OpenImages
-  )
-
-  $chart = Get-Variable -Name "$ChartName" -ValueOnly
-  
-  $chart.SaveImage($FileName,$fileformat)
-  if($OpenImages -eq $true){Invoke-Item $FileName }
-}
-
-Export-ModuleMember -Function New-PSGraphChart, Add-PSGraphSerie
+Export-ModuleMember -Function New-PSGraphChart
